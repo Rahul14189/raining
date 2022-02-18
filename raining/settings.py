7e68a6d0f810e6e1b504225264aecf1272761874
Rahul jha <rahuljharm@gmail.com>
2022-02-17 20:30:52 -0800

    2st commit

--- raining/settings.py
+++ raining/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&2rc6_vhcysyu2&^!-2yum-+&f-158dg#+k+@)_0+^q7az_4$q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hello141.herokuapp.com']
 
